110271757e616b8621a49c7f1aa23551a95941c4
Kegstand <pranavmin@gmail.com>
2026-01-15 17:38:39 -0800

    Improved extract slug logic

--- src/util.js
+++ src/util.js
@@ -2,6 +2,8 @@
 export function extractSlug(input) {
   // If they paste the whole URL, strip the domain
   if (input.includes("start.gg/")) {
+    const match = input.match(/tournament\/[^/]+\/event\/[^/]+/);
+    if (match) return match[0];
     return input.split("start.gg/")[1].split("/overview")[0].split("/brackets")[0];
   }
   // If they just typed 'tournament/name/event/game', return as is
